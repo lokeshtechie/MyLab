684574d5c33927426f7c568f0b3aad2237460864
lokeshtechie <lokeshsahoo22@gmail.com>
2021-09-05 18:15:02 +0530

    raju bangaya gentlemen

--- src/main/java/webapp/MyLab/MyLabApplication.java
+++ src/main/java/webapp/MyLab/MyLabApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyLabApplication {
-
+        #lokesh 
 	public static void main(String[] args) {
 		SpringApplication.run(MyLabApplication.class, args);
 	}
