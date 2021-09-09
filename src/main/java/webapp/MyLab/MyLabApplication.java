1e9678f23e75f8856d385967c1b5c514908c51d9
“lokeshtechie” <lokeshsahoo22@gmail.com>
2021-09-09 03:29:37 +0000

    Changes

--- src/main/java/webapp/MyLab/MyLabApplication.java
+++ src/main/java/webapp/MyLab/MyLabApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+#lokesh Sahoo
 public class MyLabApplication {
         #lokesh 
 	public static void main(String[] args) {
