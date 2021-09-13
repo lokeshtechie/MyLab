195f8f5e39e346d119a1bff06b7667993524452c
lokeshtechie <54534036+lokeshtechie@users.noreply.github.com>
2021-09-13 21:18:06 +0530

    Update MyLabApplication.java

--- src/main/java/webapp/MyLab/MyLabApplication.java
+++ src/main/java/webapp/MyLab/MyLabApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-#lokesh Sahoo
+// lokesh Sahoo
 public class MyLabApplication {
         // lokesh 
 	public static void main(String[] args) {
